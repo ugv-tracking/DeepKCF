96e9f9984ef43d518bc58460a4a81e8b761aad5c
maxtom <hitmaxtom@gmail.com>
2017-10-04 17:29:53 -0400

    ok

---
+++ test_python.py
@@ -0,0 +1,14 @@
+import numpy as np
+import scipy.misc as scpmi
+import cv2
+from matplotlib import pyplot as plt
+from util import *
+import numpy as np
+from tracker import Tracker
+from kernel_params import Params
+import sys,os
+import tensorflow as tf
+from tensorflow.contrib.slim.nets import inception
+
+def test():
+    print ("ok")
---
+++ tosimainmem.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python
+import cv2
+import numpy as np
+
+def seg(d):
+
+    print ("ok")
+
+    return masks
